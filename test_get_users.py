be28e9f91209bfd1784ff21cb0b765c74fb830f1
Aleksandr Kotlyar <ask.kotlyar@gmail.com>
2023-08-27 17:46:26 +0300

    rest api part 2

--- test_get_users.py
+++ test_get_users.py
@@ -1,14 +1,31 @@
 import json
 
-import requests
+import allure
+from allure_commons.types import AttachmentType
 from jsonschema.validators import validate
+from requests import sessions
+from curlify import to_curl
+
+
+def reqres_api(method, url, **kwargs):
+    base_url = "https://reqres.in"
+    new_url = base_url + url
+    method = method.upper()
+    with allure.step(f"{method} {url}"):
+        with sessions.Session() as session:
+            response = session.request(method=method, url=new_url, **kwargs)
+            message = to_curl(response.request)
+            allure.attach(body=message.encode("utf8"), name="Curl", attachment_type=AttachmentType.TEXT, extension='txt')
+            allure.attach(body=json.dumps(response.json(), indent=4).encode("utf8"), name="Response Json", attachment_type=AttachmentType.JSON, extension='json')
+    return response
 
 
 def test_users_per_page():
-    per_page = 'abc'
+    per_page = 2
 
-    response = requests.get(
-        url="https://reqres.in/api/users",
+    response = reqres_api(
+        "get",
+        url="/api/users",
         params={"per_page": per_page}
     )
 
@@ -21,6 +38,6 @@ def test_users_schema():
     with open('get_users_schema.json') as file:
         schema = json.loads(file.read())
 
-    response = requests.get("https://reqres.in/api/users")
+    response = reqres_api("get", "/api/users")
 
     validate(instance=response.json(), schema=schema)
---
+++ test_ui.py
@@ -0,0 +1,46 @@
+from time import sleep
+
+import requests
+from allure import step
+from selene.support.conditions import have
+from selene.support.shared import browser
+
+
+LOGIN = "example1200@example.com"
+PASSWORD = "123456"
+WEB_URL = "https://demowebshop.tricentis.com/"
+API_URL = "https://demowebshop.tricentis.com/"
+
+def test_login():
+    """Successful authorization to some demowebshop (UI)"""
+    with step("Open login page"):
+        browser.open("http://demowebshop.tricentis.com/login")
+
+    with step("Fill login form"):
+        browser.element("#Email").send_keys(LOGIN)
+        browser.element("#Password").send_keys(PASSWORD).press_enter()
+
+    with step("Verify successful authorization"):
+        browser.element(".account").should(have.text(LOGIN))
+
+
+def test_login_through_api():
+    """Successful authorization to some demowebshop (UI)"""
+    with step("Get auth cookie though API"):
+        url = API_URL + "/login"
+        auth_cookie_name = "NOPCOMMERCE.AUTH"
+        payload = {
+            'Email': LOGIN,
+            "Password": PASSWORD,
+            "RememberMe": False
+        }
+        response = requests.request("POST", url, data=payload, allow_redirects=False)
+        cookie = response.cookies.get(auth_cookie_name)
+
+    with step("Open browser with auth cookie"):
+        browser.open(WEB_URL)
+        browser.driver.add_cookie({"name": auth_cookie_name, "value": cookie})
+        browser.open(WEB_URL)
+
+    with step("Verify successful authorization"):
+        browser.element(".account").should(have.text(LOGIN))
